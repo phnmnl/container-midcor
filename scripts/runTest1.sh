3c2151ac592b9329860a6fa6e578513187d8a4bf
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-02-20 16:45:41 +0000

    Adds appropriate wget installation and fixes test.

--- scripts/runTest1.sh
+++ scripts/runTest1.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-wget "https://drive.google.com/uc?export=download&id=0B2e3YmwhK4fkeW1ORld3ZVZ2ZDQ" -O midcor_outin_input.csv
+apt-get update -y && apt-get install -y --no-install-recommends wget ca-certificates
+
+wget "https://drive.google.com/uc?export=download&id=0B7S2ZMhdzWwbX3dnc1Z3WWhTak0" -O midcor_outin_input.csv
 runMidcor.R -i midcor_outin_input.csv -o output.csv
+
+if [ ! -f output.csv ]; then
+   	echo "File output.csv does not exist, failing test."
+   	exit 1
+fi
+
+echo "midcor runs with test data without error codes, all expected files created."
