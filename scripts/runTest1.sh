21f7dc85c8ab3c8bb8f60267855e2ffdb6b41500
seliv55 <selivanov@ub.edu>
2017-07-04 21:10:51 +0200

    change inputfile

--- scripts/runTest1.sh
+++ scripts/runTest1.sh
@@ -2,7 +2,7 @@
 
 apt-get update -y && apt-get install -y --no-install-recommends wget ca-certificates
 
-wget "https://drive.google.com/uc?export=download&id=0B7S2ZMhdzWwbX3dnc1Z3WWhTak0" -O midcor_outin_input.csv
+wget "https://drive.google.com/uc?export=download&id=0B1lAg6jyw6lvSUlxd0Y2TVY0eWM" -O midcor_outin_input.csv
 runMidcor.R -i midcor_outin_input.csv -o output.csv
 rc=$?; 
 if [[ $rc != 0 ]]; then 
