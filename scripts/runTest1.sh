8d0be7b3c5162a4f9e968358955f0ad71c963dd9
seliv55 <selivanov@ub.edu>
2018-08-02 13:04:43 +0200

    flexible to input

--- scripts/runTest1.sh
+++ scripts/runTest1.sh
@@ -3,6 +3,7 @@
 apt-get update -y && apt-get install -y --no-install-recommends wget ca-certificates
 
 wget "https://drive.google.com/uc?export=download&id=0B1lAg6jyw6lvSUlxd0Y2TVY0eWM" -O midcor_outin_input.csv
+
 runMidcor.R -i midcor_outin_input.csv -o output.csv
 rc=$?; 
 if [[ $rc != 0 ]]; then 
